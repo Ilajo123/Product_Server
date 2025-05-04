5658f3020efee88c7ab8115462a33ed04cd4bc16
Lloyd Aguirre <laguirre1983@gmail.com>
2025-05-04 15:44:58 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use('/', (req,res) => {
+  res.send('My First Server');
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port`};
+});
