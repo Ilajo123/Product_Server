250bac88535b454f191a2a3335ece029246b0f12
Lloyd Aguirre <laguirre1983@gmail.com>
2025-05-04 16:24:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use('/', (req,res) => {
+app.use('/', ((req,res) => {
   res.send('My First Server');
 });
 
