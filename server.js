e941f296e18fc5bef777ae1c0fd07ce540efe2e8
Lloyd Aguirre <laguirre1983@gmail.com>
2025-05-07 14:54:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
+const logEvents = require('./middleware/logEvents');
 const PORT  = process.env.PORT || 3000;
+const cors = require('cors')
+const corsOption = require('./config/corsOptions');
+const credentials = require('./config/credentials');
 
-app.get('/', (req,res) => {
-  res.json({"message": "Tests"});
+app.use((req,res,next) => {
+  logEvents(`${req.method} - ${req.headers.origin} - ${req.url}`, 'serverLog.txt');
+  next();
+});
+app.use(express.urlencoded({extended: false}));
+app.use(credentials);
+app.use(cors(corsOption));
+app.get('/', (req,res,next) => {
+  res.send('Hi');
 });
 
 app.listen(PORT, () => {
   console.log(`Listening to PORT ${PORT}`)
 });
+
 
