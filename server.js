f2bbe844ef9c5a73a2a75d8609d6bb01ff8b273b
Lloyd Aguirre <laguirre1983@gmail.com>
2025-05-10 10:33:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT  = process.env.PORT || 3000;
 const cors = require('cors')
 const corsOption = require('./config/corsOptions');
 const credentials = require('./config/credentials');
+const path = require('path');
 
 app.use((req,res,next) => {
   logEvents(`${req.method} - ${req.headers.origin} - ${req.url}`, 'serverLog.txt');
@@ -13,12 +14,27 @@ app.use((req,res,next) => {
 app.use(express.urlencoded({extended: false}));
 app.use(credentials);
 app.use(cors(corsOption));
-app.get('/', (req,res,next) => {
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'/public')));
+app.get('/', (req,res) => {
   res.send('Hi');
 });
+
+app.use('/login', require('./routes/login.js'));
+
+app.all('/*splat', (req,res) => {
+  res.status(404);
+  if (req.accepts('html')){
+    res.sendFile(path.join(__dirname,'views','404.html'));
+  } else if(req.accepts('json')) {
+    res.json({error: "404 Page Not Found"})
+  } else {
+    res.type('txt').send("Error 404 - Page Not Found");
+  }
+  
+});
+
 
 app.listen(PORT, () => {
   console.log(`Listening to PORT ${PORT}`)
 });
-
-
